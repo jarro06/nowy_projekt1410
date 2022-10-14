c409b0eebcaaffc4efc9de1aa88ddb56c38a08db
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-10-14 11:43:43 +0200

    Create maven project

---
+++ src/main/java/de/allianz/nowy_projekt1410/NowyProjekt1410Application.java
@@ -0,0 +1,13 @@
+package de.allianz.nowy_projekt1410;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowyProjekt1410Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowyProjekt1410Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/nowy_projekt1410/NowyProjekt1410ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.nowy_projekt1410;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowyProjekt1410ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
